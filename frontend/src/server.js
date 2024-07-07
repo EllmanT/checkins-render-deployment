e14a19c31a57773f689a13f06214afeba6123ac0
EllmanT <tmuranda1@gmail.com>
2024-07-07 18:49:07 +0200

    Updated to the live server

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,6 @@
 //the server for the testing//
-export const server = "http://localhost:5000/api/v2";
+//export const server = "http://localhost:5000/api/v2";
+
+//LIVE SERVER
+export const server = "https://checkins-render-prod-deployment.onrender.com/api/v2";
 //server for the live site
