65599f948fa63131e6aab14709cab652ae9331d6
EllmanT <tmuranda1@gmail.com>
2024-07-11 13:06:03 +0200

    sorting out the socket io

--- frontend/src/pages/deliverer/VisitsPage.jsx
+++ frontend/src/pages/deliverer/VisitsPage.jsx
@@ -46,16 +46,14 @@ import { endpoint } from "socketIOEndpoint";
 
 const VisitsPage = () => {
   const { user, loading } = useSelector((state) => state.user);
-  const socket = io(); // Replace with your Socket.IO server URL
+  const socket = io("https://checkins-render-prod-deployment.onrender.com"); // Replace with your Socket.IO server URL
   //const ENDPOINT = "http://localhost:3005";
   //const socketId = socketIO(endpoint, { transports: ["websocket"] });
 
   socket.on("connect", () => {
     console.log("connected on", socket.id);
   });
-  // socket.on("update-complete", (message) => {
-  //   console.log("message from the backend", message);
-  // });
+
 
   const theme = useTheme();
   const dispatch = useDispatch();
