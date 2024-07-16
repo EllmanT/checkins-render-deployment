22060d5b02230c3e938d1ff40bc13f15d5f43db7
EllmanT <tmuranda1@gmail.com>
2024-07-16 13:06:54 +0200

    updated the image url

--- backend/controller/visit.js
+++ backend/controller/visit.js
@@ -16,6 +16,7 @@ const contractor = require("../model/contractor");
 const sendMail = require("../utils/sendMail");
 const { credentialsObject } = require("./credentialsObject");
 require("dotenv").config();
+const footerUrl = require("./footer-image.png")
 
 const spreadsheetId = process.env.VISITS_SPREADSHEET_ID;
 
@@ -264,7 +265,7 @@ We look forward to assisting you again in the future. Travel safe and have a won
 
 Best regards,
 
-<img src="./footer-image.jpg" alt="Footer Image" style="display: block; margin-top: 1rem;">
+<img src="./footer-image.png" alt="Footer Image" style="display: block; margin-top: 1rem;">
 `;
           try {
             await sendMail({
