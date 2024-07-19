a4dd85750a7be69b7fecd67288cfabfc10ecc946
EllmanT <tmuranda1@gmail.com>
2024-07-19 11:38:19 +0200

    added description

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,7 @@
 //the server for the testing//
 //export const server = "http://localhost:5000/api/v2";
 
-//LIVE SERVER
+//LIVE SERVER url to the render site
  export const server = "https://checkins-render-prod-deployment.onrender.com/api/v2";
 //export const server = "https://www.myassistant.co.zw/api/v2";
 //server for the live site
