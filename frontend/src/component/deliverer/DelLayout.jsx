f5602d5001352bd945f357c31664e3843c44348a
EllmanT <tmuranda1@gmail.com>
2024-07-28 11:49:17 +0200

    Updated the sidebar to be mobile responsive

--- frontend/src/component/deliverer/DelLayout.jsx
+++ frontend/src/component/deliverer/DelLayout.jsx
@@ -7,7 +7,7 @@ import { Outlet } from "react-router-dom";
 
 const DelLayout = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(isNonMobile);
   const { user } = useSelector((state) => state.user);
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
