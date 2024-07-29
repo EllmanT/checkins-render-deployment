cc3ba3853e66abb388c2baa8461566421bc0a554
EllmanT <tmuranda1@gmail.com>
2024-07-29 15:10:09 +0200

    Removed gatsby

--- frontend/src/component/deliverer/Navbar.jsx
+++ frontend/src/component/deliverer/Navbar.jsx
@@ -35,12 +35,11 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { server } from "server";
 import logo from "./axis-logo-neat.png"
-import { navigate } from "gatsby";
 
 
 const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
-  //const navigate= useNavigate();
+  const navigate= useNavigate();
   const theme = useTheme();
   const [anchorEl, setAchorEl] = useState(null);
   const isOpen = Boolean(anchorEl);
@@ -66,10 +65,9 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
       .get(`${server}/user/logout`, { withCredentials: true })
       .then((res) => {
         toast.success(res.data.message);
-  
-        // Add a cache buster to the URL to prevent caching
-        const cacheBuster = Date.now(); // Generate a unique timestamp
-        navigate(`/login?cb=${cacheBuster}`); // Redirect to login page with cache buster
+        navigate("/login");
+        window.location.reload(true);
+
       })
       .catch((error) => {
         console.log(error.response.data.message);
